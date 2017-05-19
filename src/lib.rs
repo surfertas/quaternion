4a8e52e320461937bc1f0c8fea4dbedb3d589093
surfertas <tasuku@gmail.com>
2017-05-19 20:39:16 +0900

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,7 @@ pub type Quaternion<T> = (T, [T; 3]);
 /// Quaternion identity quaternion
 #[inline(always)]
 pub fn id<T>() -> Quaternion<T>
-where 
-    T: Float
+    where T: Float
 {
     let one = T::one();
     let zero = T::zero();
@@ -26,12 +25,8 @@ where
 
 /// Quaternion addition
 #[inline(always)]
-pub fn add<T>(
-    a: Quaternion<T>,
-    b: Quaternion<T>
-) -> Quaternion<T>
-where 
-    T: Float
+pub fn add<T>(a: Quaternion<T>, b: Quaternion<T>) -> Quaternion<T>
+    where T: Float
 {
     let mut c: Quaternion<T> = id();
     c.0 = a.0 + b.0;
@@ -45,15 +40,11 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-        
+
     #[test]
     fn test_add() {
         let q0: Quaternion<f64> = id();
         let q1: Quaternion<f64> = (1.0, [1.0, 1.0, 1.0]);
         assert_eq!(add(q0, q1), (2.0, [1.0, 1.0, 1.0]));
     }
-}   
-        
-    
-    
-
+}
